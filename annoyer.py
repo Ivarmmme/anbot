ab9707fc1f90c36a72d9a66078a7d9d567bd00d4
Ivarmmme <162916474+Ivarmmme@users.noreply.github.com>
2024-05-19 11:35:59 +0530

    Update annoyer.py

--- annoyer.py
+++ annoyer.py
@@ -1,7 +1,6 @@
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext, filters
-from echo import echo, start_echo, stop
 # Retrieve the bot token from an environment variable
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 AUTHORIZED_USERS = [6369933143]  # Replace with actual user IDs
@@ -23,12 +22,11 @@ async def start_gif_reply(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text(f"Replying with GIFs to messages from user {user_id}.")
 
 GIF_URL = 'https://graph.org/file/6805acd7f5e08d1266dfa.mp4'  # Replace with your actual GIF URL
-gif_users = set()
 async def gif_reply(update: Update, context: CallbackContext) -> None:
     if update.message and update.message.from_user.id in gif_users:
         # Reply with a GIF using the URL
         await update.message.reply_animation(animation=GIF_URL)
-gif_users = set()
+
 async def stop_echo(update: Update, context: CallbackContext) -> None:
     if update.message.from_user.id in AUTHORIZED_USERS:
         gif_users.clear()# Clear the set of users being replied with GIFs
@@ -41,13 +39,10 @@ def main() -> None:
     application = Application.builder().token(TOKEN).build()
 
     # Command handler
-    application.add_handler(CommandHandler('echo', start_echo, filters.ChatType.GROUPS))
     application.add_handler(CommandHandler('mathi', stop_echo, filters.ChatType.GROUPS))
-    application.add_handler(CommandHandler('end', stop, filters.ChatType.GROUPS))
     application.add_handler(CommandHandler('surah', start_gif_reply, filters.ChatType.GROUPS))
 
     # Message handlers
-    application.add_handler(MessageHandler(filters.TEXT & filters.ChatType.GROUPS, echo))
     application.add_handler(MessageHandler(filters.TEXT & filters.ChatType.GROUPS, gif_reply))
 
     # Start the bot
